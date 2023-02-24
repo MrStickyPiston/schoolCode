2b8bc4b3113a70243b741822d8e8995ed2917d5b
MrStickyPiston <mr.sticky.piston@gmail.com>
2023-02-24 17:22:29 +0100

    Added zoom to mandelbrot (finally)

--- mandelbrot/__main__.py
+++ mandelbrot/__main__.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
 
 def get_iter(c: complex, thresh: int = 4, max_steps: int = 25) -> int:
@@ -11,23 +12,46 @@ def get_iter(c: complex, thresh: int = 4, max_steps: int = 25) -> int:
     return i
 
 
-def plotter(n, thresh, pos, max_steps=25):
+def plotter(n, thresh, pos, zoom, max_steps=25):
+    zoom = zoom * n / 10
+    while 2 * zoom >= n:
+        print("Double n because 2*i > n")
+        n *= 2
+
     mx = 2.48 / (n - 1)
     my = 2.26 / (n - 1)
     mapper = lambda x, y: (mx * x - 2, my * y - 1.13)
-    img = np.full((n, n), 255)
-    for x in range(n):
-        print(f"{round((x/(n*n))*100000, 2)}%")
-        for y in range(n):
-            it = get_iter(complex(*mapper(x+pos[0], y+pos[1])), thresh=thresh, max_steps=max_steps)
+    img = np.full((n - int(zoom * 2), n - int(zoom * 2)), 255)
+
+    for x in range(0, n - int(zoom * 2)):
+        sys.stdout.write(f"\rPROGRESS: {round((x / (n - zoom * 2)) * 100)}%")
+        sys.stdout.flush()
+        for y in range(0, n - int(zoom * 2)):
+            it = get_iter(complex(*mapper(x + pos[0] * n / 100 + int(zoom), y + pos[1] * n / 100 + int(zoom))),
+                          thresh=thresh, max_steps=max_steps)
             img[y][x] = it
+
     return img
 
 
-n = 1000
-pos = [0, 0]
+def main(n, pos, zoom):
+    # n = 100000
+    # pos = [0, -10]
+    # zoom = 4.5
 
-img = plotter(n, thresh=4, max_steps=50, pos=pos)
-plt.imshow(img)
-plt.axis("off")
-plt.show()
+    print("Starting mandelbrot calculation....")
+    img = plotter(n, thresh=4, max_steps=50, pos=pos, zoom=zoom)
+    print("\nMandelbrot array calculated!\nStarting GUI implementation...")
+    plt.imshow(img)
+    plt.axis("off")
+    plt.show()
+    print("GUI ready!")
+
+
+if __name__ == "__main__":
+    print("starting StickyPiston-Development mandelbrot generator...")
+    n = int(input("Enter resolution: "))
+    pos = int(input("Enter x (int): ")), int(input("Enter y (int): "))
+    zoom = float(input("Enter zoom (float): "))
+
+    main(n, pos, zoom)
